e731f33b5846871a9865d92598b6fbbb1cde04d3
Piyush Kalyan <79275157+PiyushKalyanpy@users.noreply.github.com>
2023-12-02 13:30:14 +0530

    Added algorithm

--- Week 5/1.c
+++ Week 5/1.c
@@ -77,3 +77,31 @@ wt: waiting time
 rt: remaining time
 st: start time
 */
+
+/* Algorithm
+1. **Initialization:**
+   - Define a structure `process` to store process information (PID, arrival time, burst time, completion time, turnaround time, waiting time, start time, remaining time).
+   - Create an array `p` of `n` `process` structures to represent the processes.
+   - Initialize variables `completed` to 0 (number of completed processes) and `ct` to 0 (current time).
+
+2. **Input Process Data:**
+   - Read the number of processes `n` from the user.
+   - For each process `i`:
+      - Read the arrival time `at[i]` and burst time `bt[i]` of process `i`.
+      - Set the process ID `pid[i]` to `i + 1`.
+      - Set the remaining time `rt[i]` to the burst time `bt[i]`.
+      - Set the start time `st[i]` to 0.
+
+3. **Scheduling:**
+   - While there are uncompleted processes (`completed != n`):
+      - Find the index `mini` of the process with the minimum remaining time (`rt`) among those that have arrived (`at <= ct`) and are not yet finished (`rt > 0`).
+         - If no such process exists, increment the current time `ct` and continue.
+      - Decrement the remaining time (`rt`) of the selected process `p[mini]`.
+      - If the remaining time of the selected process becomes zero (`rt == 0`), update its completion time (`ct`), turnaround time (`tat`), and waiting time (`wt`).
+         - Increment the number of completed processes (`completed`).
+      - Increment the current time `ct`.
+
+4. **Output Results:**
+   - Print a table summarizing the process information: PID, arrival time, burst time, completion time, turnaround time, and waiting time.
+   - Calculate and print the average turnaround time (`avg_tat`) and average waiting time (`avg_wt`).
+*/
